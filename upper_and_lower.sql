827ef36f89408378519e6b8353decaa1598e8b84
Mirza Sisic <mirzasisic57@gmail.com>
2018-09-01 03:47:30 +0200

    Upper and Lower

---
+++ upper_and_lower.sql
@@ -0,0 +1,12 @@
+    SELECT UPPER('Hello World');
+     
+    SELECT LOWER('Hello World');
+     
+    SELECT UPPER(title) 
+    FROM books;
+     
+    SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) 
+    FROM books;
+     
+    SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) 
+    FROM books;
