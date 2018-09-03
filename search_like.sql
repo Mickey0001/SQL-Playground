2755af0cbe7fa8c745612212f37b8fe0a37bc1cb
Mirza Sisic <mirzasisic57@gmail.com>
2018-09-03 18:21:02 +0200

    Search With LIKE Operator

---
+++ search_like.sql
@@ -0,0 +1,9 @@
+    SELECT title, author_fname FROM books WHERE author_fname LIKE '%da%';
+     
+    SELECT title, author_fname FROM books WHERE author_fname LIKE 'da%';
+     
+    SELECT title FROM books WHERE  title LIKE 'the';
+     
+    SELECT title FROM books WHERE  title LIKE '%the';
+     
+    SELECT title FROM books WHERE title LIKE '%the%';
