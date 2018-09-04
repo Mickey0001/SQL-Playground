25948efa362717d6f4117caff0f392a9acc3678f
Mirza Sisic <mirzasisic57@gmail.com>
2018-09-04 20:35:16 +0200

    CHAR and VARCHAR

---
+++ char_and_varchar.sql
@@ -0,0 +1,13 @@
+    CREATE TABLE dogs (name CHAR(5), breed VARCHAR(10));
+     
+    INSERT INTO dogs (name, breed) VALUES ('bob', 'beagle');
+     
+    INSERT INTO dogs (name, breed) VALUES ('robby', 'corgi');
+     
+    INSERT INTO dogs (name, breed) VALUES ('Princess Jane', 'Retriever');
+     
+    SELECT * FROM dogs;
+     
+    INSERT INTO dogs (name, breed) VALUES ('Princess Jane', 'Retrievesadfdsafdasfsafr');
+     
+    SELECT * FROM dogs;
