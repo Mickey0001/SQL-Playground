c4178f5216a9bac8a0fe46f5803781f3386dc550
Mirza Sisic <mirzasisic57@gmail.com>
2018-09-05 18:53:16 +0200

    Float and Double

---
+++ Float_and_double.sql
@@ -0,0 +1,13 @@
+    CREATE TABLE thingies (price FLOAT);
+     
+    INSERT INTO thingies(price) VALUES (88.45);
+     
+    SELECT * FROM thingies;
+     
+    INSERT INTO thingies(price) VALUES (8877.45);
+     
+    SELECT * FROM thingies;
+     
+    INSERT INTO thingies(price) VALUES (8877665544.45);
+     
+    SELECT * FROM thingies;
